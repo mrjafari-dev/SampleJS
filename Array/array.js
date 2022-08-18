2751c24d92416b4ee6f285be2b8eec23527c4bbb
unknown <mrjafari483@Gmail.com>
2022-08-17 23:36:27 -0700

    finding element  in Array (Refrence Type)

--- Array/array.js
+++ Array/array.js
@@ -5,9 +5,27 @@ arr.unshift(1,2);
 arr.splice(2,0,'ali','mammad');
 console.log(arr);
 
-// finding element in Array
+// finding element  in Array (Primitive)
 let numbers = [1,2,3,6,1,5,7,1,5]
 console.log(numbers.indexOf(6)); //return index of element
 console.log(numbers.indexOf(1, 5));//find element from 5 indext 
 console.log(numbers.indexOf(10) !== -1); //return true or false
 console.log(numbers.includes(5)); // return element is exicts or no (True and false)
+
+// finding element  in Array (Refrence Type)
+var corses = [
+    {id :1 ,name :'Ali'},
+    {id : 2 , name : 'Mammad'}
+]
+
+    console.log(corses.includes({id :1 ,name :'Ali'}));//=return true or falsse for find value\
+
+const value = corses.find(function(element){  // return object of find value
+        return element.id === 1;
+})
+console.log(value);
+
+const cours = corses.findIndex(function(element){  // return object of find value
+    return element.id === 2;
+})
+console.log(cours);
